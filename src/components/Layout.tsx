4ce6b50104eeee6090a52e1af483fdbdfb2cb074
Martin Kristof <martin.kristof@heureka.cz>
2023-09-15 10:45:39 +0200

    fix navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,22 @@
 import { NavLink, Outlet } from 'react-router-dom';
-import { Container, Nav, NavItem, NavLink as NavLinkReactstrap } from 'reactstrap';
+import { Container, Nav, NavItem, NavLink as NavLinkReactstrap, Navbar } from 'reactstrap';
 
 export const Layout = () => (
   <>
-    <Nav>
-      <NavItem>
-        <NavLinkReactstrap to="/" tag={NavLink}>
-          Dashboard
-        </NavLinkReactstrap>
-      </NavItem>
-      <NavItem>
-        <NavLinkReactstrap to="/heroes" tag={NavLink}>
-          Heroes
-        </NavLinkReactstrap>
-      </NavItem>
-    </Nav>
+    <Navbar expand="md">
+      <Nav navbar>
+        <NavItem>
+          <NavLinkReactstrap to="/" tag={NavLink}>
+            Dashboard
+          </NavLinkReactstrap>
+        </NavItem>
+        <NavItem>
+          <NavLinkReactstrap to="/heroes" tag={NavLink}>
+            Heroes
+          </NavLinkReactstrap>
+        </NavItem>
+      </Nav>
+    </Navbar>
     <hr />
     <Container>
       <Outlet />
