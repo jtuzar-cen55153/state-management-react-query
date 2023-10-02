fcdf2ab90700fa3028eda1ac731d0bb7abb5afd5
Martin Kristof <martin.kristof@heureka.cz>
2023-10-02 21:28:00 +0200

    add code-splitting for vendor - build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite';
+import { dependencies } from './package.json';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+
+// https://sambitsahoo.com/blog/vite-code-splitting-that-works.html
+const renderChunks = (deps: Record<string, string>) => {
+  let chunks = {};
+  Object.keys(deps).forEach(key => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 });
